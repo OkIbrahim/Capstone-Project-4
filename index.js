316b85b04bf8e12353beecf245a878d39bb5968b
OkIbrahim <155417872+OkIbrahim@users.noreply.github.com>
2024-12-29 20:30:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+    try {
+       const result = await axios.get("https://v2.jokeapi.dev/joke/Any?format=txt");
+       res.render("index.ejs");
+    } catch (error) {
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
